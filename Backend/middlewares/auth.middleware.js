4484ef5525c3bb0a9a794a80976d0260dff5477f
Manav Kaneriya <mkaneriya373@gmail.com>
2025-02-26 19:06:35 +0530

    Backend: bug fixed

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,4 +1,3 @@
-const userModel = require('../models/user.model');
 const jwt = require('jsonwebtoken')
 
 module.exports.authUser = async (req, res, next) => {
@@ -9,12 +8,8 @@ module.exports.authUser = async (req, res, next) => {
         }
 
         const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
-        const user = await userModel.findById(decoded._id);
-        if (!user) {
-            return res.status(401).json({ message: "Unauthorized" });
-        }
 
-        req.user = user;
+        req.user = decoded;
         next();
 
     } catch (err) {
